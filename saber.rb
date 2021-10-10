d6bbf986b54b787a895cc581a5cd6e7f529e51a9
Ivan Reyes <ivanwilf@hotmail.com>
2021-10-10 15:46:39 +0900

    deleted secrets

--- saber.rb
+++ saber.rb
@@ -10,9 +10,9 @@ module Saber
       uri = URI.parse("https://api.line.me/v2/bot/message/push")
       request = Net::HTTP::Post.new(uri)
       request.content_type = "application/json"
-      request["Authorization"] = "Bearer {FZhcGunvoqhbnU9abAc9yqhOi+a/VuyAHudL3O/akEC7/0Hw5LUv3VFgiQrjEmHqWt9rRD5XCV0lrFirS0FajizIEz64SskEwdh0ZqsAu9FnqDoNanJzNiIPM0tHhItYAtju982Ksc91izsK+r3GJQdB04t89/1O/w1cDnyilFU=}"
+      request["Authorization"] = "Bearer {#{ENV[]}}"
       request.body = JSON.dump({
-        "to" => "Ua008b6426e2f49a5f08b16e8702e9ace",
+        "to" => "#{ENV[]}",
         "messages" => [
           {
             "type" => "text",
