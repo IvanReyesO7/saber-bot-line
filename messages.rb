c245de429c680f7fecbe2c798269e5f3b9b11868
Ivan Reyes <ivanwilf@hotmail.com>
2021-10-11 09:27:31 +0900

    Fixed date, github server is in UTC and I'm located in Japan

--- messages.rb
+++ messages.rb
@@ -4,7 +4,7 @@ require_relative 'weather'
 module Saber
   class LineMessages
     def self.morning
-      message = "Buenos dias gordos! hoy es #{Date.today.strftime('%b %d, %Y')}\n"
+      message = "Buenos dias gordos! hoy es #{(Date.today + 1).strftime('%b %d, %Y')}\n"
       message += fetch_weather + "\n"
       message += "Cuidense mucho 😘"
       return message
