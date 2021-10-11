11f4d3cef59a49bbf6c63ad0cbc1aa0f65d64387
Ivan Reyes <ivanwilf@hotmail.com>
2021-10-11 11:44:21 +0900

    Fixed typos in message

--- messages.rb
+++ messages.rb
@@ -4,9 +4,10 @@ require_relative 'weather'
 module Saber
   class LineMessages
     def self.morning
-      message = "Buenos dias gordos! hoy es #{(Date.today + 1).strftime('%b %d, %Y')}\n"
+      message = "Buenos días gordos ☀️ \n"
+      message += "hoy es #{(Date.today + 1).strftime('%b %d de %Y')}\n"
       message += fetch_weather + "\n"
-      message += "Cuidense mucho 😘"
+      message += "Cuídense mucho 😘"
       return message
     end
   end
